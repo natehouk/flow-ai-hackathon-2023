d2439c2e6020e76ec607e71e91a8cdabdbc2b4e5
Nate Houk <nate.houk@gmail.com>
2023-06-10 20:10:32 +0200

    Refactor

--- streaming/urls.py
+++ streaming/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('latest-data/', get_data, name='latest-data'),
     path('latest-summary/', get_summary, name='latest-summary'),
     path('post-prompt/', post_prompt, name='post-prompt'),
-    path('index/', index, name='index'),
+    path('', index, name='index'),
 ]
