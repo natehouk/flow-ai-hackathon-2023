22cd684f73beefd8c7f47e4e37b4287fbaf11914
ayushshivani <ayushshivani12345@gmail.com>
2023-06-10 23:50:16 +0200

    added marketaux

---
+++ streaming/management/commands/marketaux.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def get_top_headlines_from_newsapi(params=""):
+    endpoint = "https://api.marketaux.com/v1/news/all"
+    query_input = ""
+    api_token = "x4ZhlBIav8w8VhTLB3cDEo4CnyVRDQJiNwwVVIwq"
+    query_params = "?entity_types=index,equity" + "&api_token="+ api_token 
+    articles = json.loads(requests.get(endpoint+query_params).text)
+    new_headlines = set()
+    for headline in articles["data"]:
+        title = headline["title"]
+        description = headline["description"].replace("\n"," ").replace("  "," ")
+        source = headline["source"]
+        new_headlines.add(str({'title':title,"description":description,"source":source}))
+    return new_headlines
+
+def request_headlines(previous_headline):
+    current_headline = set()
+    new_headline = set()
+    current_headline = get_top_headlines_from_newsapi(params="")
+    new_headline = current_headline - previous_headline
+    return new_headline
