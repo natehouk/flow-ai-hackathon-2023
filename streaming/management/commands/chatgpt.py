f25d0bf9693974f45f5f800897c1682ffac0cecd
Nate Houk <nate.houk@gmail.com>
2023-06-10 19:18:46 +0200

    Refactor

--- streaming/management/commands/chatgpt.py
+++ streaming/management/commands/chatgpt.py
@@ -13,7 +13,11 @@ def get_prompt(prompt):
 	print(os.environ.get("OPENAI_API_KEY", "sk-"))
 	chat_completion = openai.ChatCompletion.create(
 	    model="gpt-3.5-turbo", messages=[
+<<<<<<< HEAD
 	    	{"role": "system", "content": SYSTEM_PROMPT},
+=======
+	    	{"role": "system", "content": f'You are a snarky news analyst at Goldman Saches. Your job is to summarize the data inputs you receive. Be concise. Show most important information at the top. Highlight important parts in bold using Markdown. Use bullet points. Display information, in plain facts, and statements.'},
+>>>>>>> 3c83ec7 (Refactor)
 	    	{"role": "user", "content": f"{prompt} \n {last_object.value}"}
 	    ], 
 	    max_tokens = 100,
