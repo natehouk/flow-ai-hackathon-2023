f9a71b4f0a65be98213f45ab32795ad901aae4b8
Nate Houk <nate.houk@gmail.com>
2023-06-11 11:17:08 +0000

    Refactor

--- textstreaming/settings.py
+++ textstreaming/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6&km4ps0f0-x@ti9hc8_$b2xv-ntq+(+a=co3%zfmivhrwml^='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["143.42.16.52", "transfer.fm", "yass.work"]
 
