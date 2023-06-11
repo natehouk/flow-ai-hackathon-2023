cb2a9741141af35cf1ee8772a7623e4c47e6dd27
Nate Houk <nate.houk@gmail.com>
2023-06-11 14:24:11 +0200

    Refactor

--- streaming/management/commands/chatgpt.py
+++ streaming/management/commands/chatgpt.py
@@ -9,8 +9,9 @@ def get_prompt(prompt):
 	last_object = Prompts.objects.last()
 
 	openai.api_key = os.environ.get("OPENAI_API_KEY")
-	gpt_model = os.environ.get("GPT_MODEL")
-	system_prompt =  os.environ.get("SYSTEM_PROMPT")
+	gpt_model = os.environ.get("GPT_MODEL", "gpt-3.5-turbo")
+	system_prompt =  os.environ.get("SYSTEM_PROMPT", "Follow these rules: 1) Your job is to summarize data inputs into bullet points. 2) Be concise. 3) Always show most important information as the top bullet point. 4) Emphasize important parts in bold using Markdown. 5) Companies, names and proper nouns should be in italic using Markdown. 6) Never write more than five bullet points. 7) The bullet points, bold and italic should be formatted using Markdown.")
+	print(system_prompt)
 	chat_completion = openai.ChatCompletion.create(
 	    model=gpt_model, messages=[
 	    	{"role": "system", "content": system_prompt},
